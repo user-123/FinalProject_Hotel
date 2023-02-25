0dfd3ebdc8d828407cac659b6773a89123b46e98
user_123 <11405033+user-123@users.noreply.github.com>
2023-02-25 18:15:00 +0800

    Order Controller Update

--- src/main/java/idv/hotel/finalproject/controller/OrderController.java
+++ src/main/java/idv/hotel/finalproject/controller/OrderController.java
@@ -3,6 +3,7 @@ package idv.hotel.finalproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import idv.hotel.finalproject.service.OrderService;
 
@@ -11,7 +12,7 @@ public class OrderController {
 	@Autowired
 	private OrderService oService;
 	
-	@GetMapping("/order/checkroominstock")
+	@PostMapping("/order/checkroominstock")
 	public String checkRoomInStock(Model model) {		
 		return "messages/addMessage";
 		//應該return boolean，待修改
